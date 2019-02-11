90f71012ebd90ac28a0c3228fa9827773ad56ba5
benjaminMunoz <benjamin.munoz@blackswan.com>
2019-02-10 16:59:05 -0800

    Feat: Add network class

---
+++ app/src/main/java/com/example/bakingappudacity/network/Network.java
@@ -0,0 +1,6 @@
+package com.example.bakingappudacity.network;
+
+public class Network {
+    public static  String baseUrl = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
+
+}
