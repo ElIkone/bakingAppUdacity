d196634b6f1b7f18863107a0aeabf41cec370899
benjaminMunoz <benjamin.munoz@blackswan.com>
2019-02-10 16:58:35 -0800

    Feat: Add main fragment class

---
+++ app/src/main/java/com/example/bakingappudacity/MainFragment.java
@@ -0,0 +1,22 @@
+package com.example.bakingappudacity;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment {
+    //Mandatory constructor for instantiating the fragment
+
+    public MainFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.main_fragment, container, false);
+
+        return rootView;
+    }
+}
