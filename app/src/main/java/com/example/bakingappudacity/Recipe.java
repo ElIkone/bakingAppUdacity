2cae1832d1d6d419034fe7b9ea2efa74c78e8751
Benjamin Munoz <elmaildebenja@gmail.com>
2019-02-11 17:27:20 -0800

    Fix: Add set and get for recipe class

--- app/src/main/java/com/example/bakingappudacity/Recipe.java
+++ app/src/main/java/com/example/bakingappudacity/Recipe.java
@@ -39,4 +39,20 @@ public class Recipe implements Parcelable {
         parcel.writeString(id);
         parcel.writeString(name);
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
