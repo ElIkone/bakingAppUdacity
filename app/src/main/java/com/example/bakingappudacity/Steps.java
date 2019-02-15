292cc9f361790a1e93c29cffc57cdc281c9c7833
Benjamin Munoz <elmaildebenja@gmail.com>
2019-02-15 10:27:58 -0800

    Feat: Add steps class

---
+++ app/src/main/java/com/example/bakingappudacity/Steps.java
@@ -0,0 +1,8 @@
+package com.example.bakingappudacity;
+
+public class Steps {
+    private int id;
+    private String shortDescription;
+    private String videoURL;
+    private String thumbnailURL;
+}
