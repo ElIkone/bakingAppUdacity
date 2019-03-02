da847e1b62065bd0e671c1ac003029b358ef8946
Benjamin Munoz <elmaildebenja@gmail.com>
2019-03-02 12:12:34 -0800

    Fix: Add getter and setter functions, convert class into parceable

--- app/src/main/java/com/example/bakingappudacity/Steps.java
+++ app/src/main/java/com/example/bakingappudacity/Steps.java
@@ -1,8 +1,75 @@
 package com.example.bakingappudacity;
 
-public class Steps {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Steps implements Parcelable {
+    public  static final Parcelable.Creator<Steps> CREATOR
+            = new Parcelable.Creator<Steps>() {
+        public  Steps createFromParcel(Parcel in) {
+            return new Steps(in);
+        }
+        public  Steps[] newArray(int size) {
+            return new Steps[size];
+        }
+    };
+
     private int id;
     private String shortDescription;
     private String videoURL;
     private String thumbnailURL;
+
+    public Steps(Parcel in) {
+        id = in.readInt();
+        shortDescription = in.readString();
+        videoURL = in.readString();
+        thumbnailURL = in.readString();
+    }
+    public Steps() { }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getVideoURL() {
+        return videoURL;
+    }
+
+    public void setVideoURL(String videoURL) {
+        this.videoURL = videoURL;
+    }
+
+    public String getThumbnailURL() {
+        return thumbnailURL;
+    }
+
+    public void setThumbnailURL(String thumbnailURL) {
+        this.thumbnailURL = thumbnailURL;
+    }
+
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(id);
+        parcel.writeString(shortDescription);
+        parcel.writeString(videoURL);
+        parcel.writeString(thumbnailURL);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
 }
